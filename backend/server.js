c0f093fc9a4581e434e131866db80004b76c27a9
Pratyush <singhpratyush981@gmail.com>
2026-01-15 19:57:37 +0530

    Updated CORS

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import cookieParser from "cookie-parser";
 const URL=process.env.FRONTEND_URL
 app.use(cookieParser());
 app.use(cors({
-  origin: true, 
+  origin: URL, 
   credentials: true
 }));
 dotenv.config()
