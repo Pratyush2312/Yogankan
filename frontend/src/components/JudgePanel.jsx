41b426d8e80d1f8d0794ca39446f8b4e2c02a9d2
Pratyush <singhpratyush981@gmail.com>
2026-01-24 22:25:18 +0530

    Minor Changes

--- frontend/src/components/JudgePanel.jsx
+++ frontend/src/components/JudgePanel.jsx
@@ -143,11 +143,11 @@ const JudgePanel = () => {
                         onChange={(e) => setStudentId(e.target.value)}
                         className="border px-3 py-2 rounded-xl w-full sm:w-32"
                     />
-                    <label>Drop</label>
                     <input type="checkbox" checked={drop} onChange={(e) => {
                         setDrop(e.target.checked)
                         console.log(drop)
                     }} />
+                    <label>Drop:</label>
                     <p>{drop ? "Yes" : "No"}</p>
                 </div>
 
